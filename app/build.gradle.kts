24f6c84e3d6ad9694529d8b40693841b8297585c
Manuel <manuelarellano0809@gmail.com>
2026-05-23 13:28:06 -0600

    Implementacion de Firebase para Notas, Tareas y Metas. Correccion 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -84,7 +85,6 @@ dependencies {
     implementation(libs.retrofit.gson)
 
 
-    // Compose UI Testing
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
@@ -94,7 +94,7 @@ dependencies {
     // AndroidX Test
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:33.1.0")) // Ajusta a la versión que uses
+    implementation(platform("com.google.firebase:firebase-bom:34.13.0"))
     implementation("com.google.firebase:firebase-firestore")
 
 }
